6cce8f84c350e93823f9bbb0fa7110bac9c1c73c
Alena Gusakov <agusakov@udel.edu>
2020-07-29 17:16:13 -0400

    attempted exercises

--- src/exercises/chapter_1/section_B.lean
+++ src/exercises/chapter_1/section_B.lean
@@ -2,6 +2,8 @@ import definitions
 
 universes u v w
 
+namespace vector_space
+
 -- 1.) Prove that -(-v) = v for every v ∈ V
 lemma neg_neg' (F : Type u) (α : Type v) [field F] [add_comm_group α] [vector_space F α] : 
     ∀ v : α, - (- v) = v :=
@@ -17,5 +19,29 @@ lemma zero_or_zero (F : Type u) (α : Type v) [field F] [add_comm_group α] [vec
     ∀ a : F, ∀ v : α, a • v = 0 → (a = 0) ∨ (v = 0) :=
 begin
     intros a v hyp,
+    sorry,
+end
+
+-- 3.) Suppose v, w ∈ V. Explain why there exists a unique x ∈ V such that v + 3x = w.
+
+-- 4.) maybe skip this one
+
+-- 5.) Show that in the definition of a vector space, the additive inverse condition can be replaced with the condition that 0v = 0 for all v ∈ V.
+
+-- maybe replace add_comm_group with something else? idk
+-- this works in just commutative groups, not just vector spaces.
+-- simplify maybe? make it an instance of add_comm_group?
+lemma zero_smul_zero_iff_add_inv (F : Type u) (α : Type v) [field F] [add_comm_group α] [vector_space F α] :
+    (∀ v : α, (0 : F) • v = 0) ↔ (∀ x : α, ∃ y : α, x + y = 0) :=
+begin 
+    split,
+    intro hyp,
+    intro v,
+    specialize hyp v,
+    rw ← add_neg_self (1 : F) at hyp,
+    rw add_smul at hyp,
+    rw one_smul at hyp,
+    rw ← hyp,
+    use (((-1) : F) • v),
     sorry,
 end
